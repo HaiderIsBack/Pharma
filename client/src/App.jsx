5846519409668f8a0f4490a361e63b55ce920ed9
Syed Zulqarnain Haider <haidershahpak17@gmail.com>
2024-10-20 21:35:20 +0500

    Lazy Load Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,55 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
 // Components
 import Nav from './components/Nav';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
+const Footer = lazy(() => import("./components/Footer"));
 import Transition from './components/Transition';
-import Cart from './components/Cart';
-import Overlay from './components/Overlay';
-import MenuSidebar from './components/MenuSidebar';
+const Cart = lazy(() => import('./components/Cart'));
+const Overlay = lazy(() => import('./components/Overlay'));
+// import Overlay from './components/Overlay';
+const MenuSidebar = lazy(() => import('./components/MenuSidebar'));
+// import MenuSidebar from './components/MenuSidebar';
 
 // Pages
 import Home from './pages/Home';
-import ProductPage from './pages/ProductPage';
+const ProductPage = lazy(() => import('./pages/ProductPage'));
+// import ProductPage from './pages/ProductPage';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
 
   return (
     <>
+    <Suspense fallback={<p>Loading</p>}>
       <Overlay />
+    </Suspense>
 
+    <Suspense fallback={<p>Loading</p>}>
       <Transition />
+    </Suspense>
 
       <Router>
         <ScrollToTop />
-        <MenuSidebar />
-        <Cart />
+        <Suspense fallback={<p>Loading</p>}>
+          <MenuSidebar />
+        </Suspense>
+        <Suspense fallback={<p>Loading</p>}>
+          <Cart />
+        </Suspense>
         <Nav />
         <Routes>
           <Route path='/'>
             <Route path='' element={<Home />} />
-            <Route path='product/:productId' element={<ProductPage />} />
+            <Route path='product/:productId' element={<Suspense fallback={<p>Loading</p>}><ProductPage /></Suspense>} />
           </Route>
         </Routes>
       </Router>
-      <Footer />
+      <Suspense fallback={<p>Loading Footer</p>}>
+        <Footer />
+      </Suspense>
     </>
   )
 }
